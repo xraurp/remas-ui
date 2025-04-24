ff752cb2ffa9486961486c5380b4d2da262bd514
Pavel Raur <pavel.raur@gmail.com>
2025-04-24 12:08:21 +0200

    Added task list page

---
+++ src/components/task-components/TaskItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-card class="q-gutter-md q-pa-md">
+    <q-card-section>
+      <div class="text-h6">{{ props.task.name }}</div>
+      <q-separator />
+      <div class="row">
+        <div class="col">Start time:</div>
+        <div class="col">
+          {{ formatDatetime(props.task.start_time) }}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">End time:</div>
+        <div class="col">
+          {{ formatDatetime(props.task.end_time) }}
+        </div>
+      </div>
+      <div class="row" v-if="props.task.status">
+        <div class="col">Status:</div>
+        <div class="col">
+          {{ props.task.status }}
+        </div>
+      </div>
+      <div class="row" v-if="props.task.owner && props.show_owner">
+        <div class="col">Task owner:</div>
+        <div class="col">{{ props.task.owner.name }}</div>
+      </div>
+      <q-separator v-if="props.task.description" />
+      <div v-if="props.task.description">
+        <div>Description:</div>
+        <div>
+          {{ props.task.description }}
+        </div>
+      </div>
+      <!--
+        TODO - add resource allocation display (node and allocated resources)
+      -->
+      <q-separator v-if="props.task.tags?.length" />
+      <div v-if="props.task.tags?.length">
+        <div class="text-subtitle">Tags:</div>
+        <div>
+          <template v-for="tag in props.task.tags">
+            <q-chip
+              v-if="tag.id"
+              :key="tag.id"
+              :label="tag.name"
+              color="primary"
+            />
+          </template>
+        </div>
+      </div>
+    </q-card-section>
+    <q-card-actions align="around">
+      <slot name="actions"></slot>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { date } from 'quasar';
+import type { Task } from '../db_models';
+import { dateFormat } from '../calendarDateFormat';
+
+const props = defineProps<{
+  task: Task;
+  show_owner: boolean;
+}>();
+
+function formatDatetime(datetime: string) {
+  return date.formatDate(new Date(datetime), dateFormat);
+}
+</script>
--- src/pages/TaskList.vue
+++ src/pages/TaskList.vue
@@ -3,16 +3,81 @@
     <div class="text-h3">Tasks</div>
     <q-separator class="q-mb-md" />
     <q-btn color="green" label="Add task" @click="addTask" />
-    <div class="q-pa-md row items-start q-gutter-md"></div>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <template v-for="item in tasks">
+        <task-item
+          v-if="item?.id"
+          :key="item.id"
+          :task="item"
+          :show_owner="false"
+          style="min-height: 200px; min-width: 400px"
+        >
+          <template v-slot:actions>
+            <q-btn flat color="primary" @click="editTask(item)">
+              Reschedule task
+            </q-btn>
+            <q-btn flat color="negative" @click="deleteTask(item)">
+              Cancel task
+            </q-btn>
+          </template>
+        </task-item>
+      </template>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import TaskItem from 'src/components/task-components/TaskItem.vue';
+import type { Task } from 'src/components/db_models';
+import { useTaskStore } from 'src/stores/task-store';
+import { getMessageFromError } from 'src/components/aux_functions';
+import { computed, onMounted } from 'vue';
+import { useQuasar } from 'quasar';
 
 const router = useRouter();
+const taskStore = useTaskStore();
+const $q = useQuasar();
+
+const tasks = computed(() => taskStore.tasks);
+
+onMounted(async () => {
+  try {
+    if (!taskStore.getTasks.length) {
+      await taskStore.fetchTasks();
+    }
+  } catch (error) {
+    if (process.env.debug) {
+      console.log(error);
+    }
+    $q.notify({
+      type: 'negative',
+      message: getMessageFromError(error, 'Failed to fetch tasks!'),
+    });
+  }
+});
 
 async function addTask() {
   await router.push({ name: 'task', params: { id: 'new' } });
+}
+
+async function editTask(task: Task) {
+  await router.push({ name: 'task', params: { id: task.id } });
+}
+
+async function deleteTask(task: Task) {
+  try {
+    if (task.id) {
+      await taskStore.deleteTask(task.id);
+    }
+  } catch (error) {
+    if (process.env.debug) {
+      console.log(error);
+    }
+    $q.notify({
+      type: 'negative',
+      message: getMessageFromError(error, 'Failed to delete task!'),
+    });
+  }
 }
 </script>
